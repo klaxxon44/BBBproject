2e2e6cb0bc4869cad0653bcbe337ec1b5ecc69c3
ヒロヅ <147971942+klaxxon44@users.noreply.github.com>
2024-12-08 11:19:24 +0200

    Add files via upload

---
+++ GGGG.js
@@ -0,0 +1,24 @@
+function hide()  {
+    let.section = document.querySelectorAll (.section)
+    section.for Each (s => {
+        s.style.display = 'none'
+    }
+        )
+        
+    }
+}
+
+function.showssection (section_id) {
+    let.section = document.getElementsByName(action_id)
+    if (section) {
+        section.style.display = 'block';
+    }
+    
+
+
+}
+
+function.final(section_id) {
+    showssection ()
+    showssection (section_id)
+}
